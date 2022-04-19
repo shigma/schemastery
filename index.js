070ab267b0f9a3cb7cf90d0fa28b9f7fa5c8747f
Shigma <shigma10826@gmail.com>
2022-04-19 12:14:26 +0800

    use tikloot

--- index.js
+++
@@ -1,5 +0,0 @@
-const { default: Schema, ...utils } = require('./lib/node')
-
-module.exports = Schema
-module.exports.default = Schema
-Object.assign(module.exports, utils)
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,4 @@
-export type Dict<T = any, K extends string = string> = { [key in K]?: T }
-export type Intersect<U> = (U extends any ? (arg: U) => void : never) extends ((arg: infer I) => void) ? I : never
-
-export function isNullable(value: any) {
-  return value === null || value === undefined
-}
-
-export function isPlainObject(data: any) {
-  return data && typeof data === 'object' && !Array.isArray(data)
-}
-
-export function valueMap<T, U>(object: Dict<T>, transform: (value: T, key: string) => U): Dict<U> {
-  return Object.fromEntries(Object.entries(object).map(([key, value]) => [key, transform(value, key)]))
-}
-
-export function clone<T>(source: T): T
-export function clone(source: any) {
-  if (!source || typeof source !== 'object') return source
-  if (Array.isArray(source)) return source.map(clone)
-  if (source instanceof Date) return new Date(source.valueOf())
-  if (source instanceof RegExp) return new RegExp(source.source, source.flags)
-  return valueMap(source, clone)
-}
+import { clone, Dict, Intersect, isNullable, isPlainObject, valueMap } from 'tikloot'
 
 interface Schema<S = any, T = S> extends Schema.Base<T> {
   (data: S): T
@@ -453,4 +431,4 @@ defineMethod('intersect', ['list'], ({ list }) => {
 
 defineMethod('transform', ['inner', 'callback'], ({ inner }, isInner) => inner.toString(isInner))
 
-export default Schema
+export = Schema
