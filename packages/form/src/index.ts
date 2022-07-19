66c64aea206b2119ba9b8652ea80c2265eb82242
Shigma <shigma10826@gmail.com>
2022-07-20 00:35:26 +0800

    feat: support form component

---
+++ packages/form/src/index.ts
@@ -0,0 +1,12 @@
+import { App } from 'vue'
+import Form from './form.vue'
+import Markdown from './markdown.vue'
+import Schema from './schema.vue'
+
+export * from './utils'
+
+export default function (app: App) {
+  app.component('k-form', Form)
+  app.component('k-markdown', Markdown)
+  app.component('k-schema', Schema)
+}
