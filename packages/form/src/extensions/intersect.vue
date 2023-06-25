761e107b1524f5664d0f8a8bb152502f1b2b02fb
Shigma <shigma10826@gmail.com>
2023-06-26 03:48:41 +0800

    fix: fix intersect within dict, close #50

--- packages/form/src/extensions/intersect.vue
+++ packages/form/src/extensions/intersect.vue
@@ -1,14 +1,26 @@
 <template>
+  <schema-base v-if="extra?.foldable" v-bind="$attrs">
+    <template #title><slot name="title"></slot></template>
+    <template #desc>
+      <slot name="desc">
+        <k-markdown :source="description"></k-markdown>
+      </slot>
+    </template>
+    <template #menu><slot name="menu"></slot></template>
+    <template #prefix><slot name="prefix"></slot></template>
+    <template #suffix><slot name="suffix"></slot></template>
+  </schema-base>
+
   <k-schema
     v-for="(item, index) in getChoices(schema)"
     :key="index"
     :modelValue="modelValue"
     @update:modelValue="$emit('update:modelValue', $event)"
-    :schema="{ ...item, meta: { ...schema.meta, ...item.meta } }"
+    :schema="extra?.foldable ? item : { ...item, meta: { ...schema.meta, ...item.meta } }"
     :initial="initial"
     :disabled="disabled"
     :prefix="prefix"
-    :extra="{ foldable: false, ...extra }"
+    :extra="{ foldable: false }"
   >
     <template #title><slot name="title"></slot></template>
     <template #prefix><slot name="prefix"></slot></template>
@@ -18,14 +30,15 @@
 
 <script lang="ts" setup>
 
-import { PropType } from 'vue'
-import { getChoices, Schema } from '../utils'
+import { computed, PropType } from 'vue'
+import { getChoices, Schema, useI18nText } from '../utils'
+import SchemaBase from '../base.vue'
 
 defineOptions({
   inheritAttrs: false,
 })
 
-defineProps({
+const props = defineProps({
   schema: {} as PropType<Schema>,
   modelValue: {} as PropType<any>,
   disabled: {} as PropType<boolean>,
@@ -35,5 +48,9 @@ defineProps({
 })
 
 defineEmits(['update:modelValue'])
+
+const tt = useI18nText()
+
+const description = computed(() => tt(props.schema.meta.description))
 
 </script>
