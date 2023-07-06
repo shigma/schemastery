ad39fff9eb42cb1857ca599d462e21d84499595c
Shigma <shigma10826@gmail.com>
2023-07-06 19:16:47 +0800

    feat: support client regexp hint, fix koishijs/koishi#1104

--- packages/form/src/icons/index.ts
+++ packages/form/src/icons/index.ts
@@ -5,5 +5,6 @@ import IconEllipsis from './ellipsis.vue'
 import IconExternal from './external.vue'
 import IconEyeSlash from './eye-slash.vue'
 import IconEye from './eye.vue'
+import IconInvalid from './invalid.vue'
 
-export { IconArrowDown, IconArrowUp, IconClose, IconEllipsis, IconExternal, IconEyeSlash, IconEye }
+export { IconArrowDown, IconArrowUp, IconClose, IconEllipsis, IconExternal, IconEyeSlash, IconEye, IconInvalid }
--- packages/form/src/primitive.vue
+++ packages/form/src/primitive.vue
@@ -24,16 +24,23 @@
       v-else-if="['date', 'datetime'].includes(schema.meta.role)" :type="schema.meta.role" v-model="date"
       @focus="$emit('focus', $event)" @blur="$emit('blur', $event)"
     ></el-date-picker>
-    <el-input v-else v-model="config" :disabled="disabled" :class="{ nullable }"
+    <el-input v-else v-model="config" :disabled="disabled" :class="{ minimal, nullable, invalid }"
       :style="{ width: minimal ? '100%' : isLink ? '16rem' : '12rem' }" :type="type"
       @focus="$emit('focus', $event)" @blur="$emit('blur', $event)">
       <template #prefix v-if="nullable"></template>
-      <template #suffix v-if="isLink">
-        <icon-external @click="onClickExternal(config)"></icon-external>
-      </template>
-      <template #suffix v-else-if="schema.meta.role === 'secret'">
-        <icon-eye v-if="showPass" @click="showPass = !showPass"></icon-eye>
-        <icon-eye-slash v-else @click="showPass = !showPass"></icon-eye-slash>
+      <template #suffix>
+        <span class="suffix-icon" v-if="isLink">
+          <icon-external @click="onClickExternal(config)"></icon-external>
+        </span>
+        <span class="suffix-icon" v-else-if="schema.meta.role === 'secret'">
+          <icon-eye v-if="showPass" @click="showPass = !showPass"></icon-eye>
+          <icon-eye-slash v-else @click="showPass = !showPass"></icon-eye-slash>
+        </span>
+        <el-tooltip v-if="invalid" :content="t(...invalid)">
+          <span class="suffix-icon">
+            <icon-invalid class="invalid"></icon-invalid>
+          </span>
+        </el-tooltip>
       </template>
     </el-input>
   </template>
@@ -58,10 +65,13 @@
 <script lang="ts" setup>
 
 import { computed, PropType, ref } from 'vue'
-import { IconExternal, IconEye, IconEyeSlash } from './icons'
+import { useI18n } from 'vue-i18n'
+import { IconExternal, IconEye, IconEyeSlash, IconInvalid } from './icons'
 import { isNullable } from 'cosmokit'
-import { useI18nText, useModel } from './utils'
+import { explain, useI18nText, useModel } from './utils'
 import Schema from 'schemastery'
+import zhCN from './locales/zh-CN.yml'
+import enUS from './locales/en-US.yml'
 
 const emit = defineEmits(['update:modelValue', 'focus', 'blur'])
 
@@ -79,7 +89,7 @@ const config = useModel()
 const tt = useI18nText()
 
 const nullable = computed(() => isNullable(config.value))
-
+const invalid = computed(() => explain(props.schema, config.value))
 const isLink = computed(() => ['url', 'link'].includes(props.schema.meta.role))
 
 const type = computed(() => {
@@ -123,19 +133,49 @@ function onClickExternal(value: string) {
   open(value, '_blank')
 }
 
+const { t, setLocaleMessage } = useI18n({
+  messages: {
+    'zh-CN': zhCN,
+    'en-US': enUS,
+  },
+})
+
+if (import.meta.hot) {
+  import.meta.hot.accept('./locales/zh-CN.yml', (module) => {
+    setLocaleMessage('zh-CN', module.default)
+  })
+  import.meta.hot.accept('./locales/en-US.yml', (module) => {
+    setLocaleMessage('en-US', module.default)
+  })
+}
+
 </script>
 
 <style lang="scss">
 
 .k-schema-item {
   .el-input {
+    .suffix-icon {
+      display: flex;
+      width: 1rem;
+      justify-content: center;
+      align-items: center;
+      margin-left: 6px;
+    }
+
     .k-icon {
       color: var(--k-text-light);
       transition: var(--color-transition);
       cursor: pointer;
+      height: 0.75rem;
 
       &:hover {
         color: var(--k-text-dark);
+      }
+
+      &.invalid {
+        cursor: auto;
+        color: var(--k-color-danger);
       }
     }
 
@@ -146,6 +186,14 @@ function onClickExternal(value: string) {
       width: 40%;
       border-top: var(--el-border);
       transition: var(--color-transition);
+    }
+    
+    .el-input__wrapper {
+      padding-right: 9px;
+    }
+
+    &:not(.minimal).invalid .el-input__wrapper {
+      box-shadow: 0 0 0 1px var(--k-color-danger) inset;
     }
   }
 
